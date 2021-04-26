4bbf8e68113281ee51f1220ee3713c01ca266de2
Luu Van Hieu <18520740@gm.uit.edu.vn>
2021-04-27 00:44:18 +0700

    tesssssst

---
+++ app/src/main/java/com/example/magicnote1/Add_Diary_2Activity.java
@@ -0,0 +1,14 @@
+package com.example.magicnote1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Diary_2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__diary_2);
+    }
+}
