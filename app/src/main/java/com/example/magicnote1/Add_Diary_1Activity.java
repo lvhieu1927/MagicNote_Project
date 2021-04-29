c56c798c8d1ebcce9928fad45e25f977b4c4583a
Luu Van Hieu <18520740@gm.uit.edu.vn>
2021-04-30 00:32:02 +0700

    sửa lại code cho đẹp

--- app/src/main/java/com/example/magicnote1/Add_Diary_1Activity.java
+++ app/src/main/java/com/example/magicnote1/Add_Diary_1Activity.java
@@ -14,14 +14,46 @@ import java.util.GregorianCalendar;
 public class Add_Diary_1Activity extends AppCompatActivity {
 
     TextView hello_text;
-    Button button1;
-    Button button2;
-    Button button3;
-    Button button4;
-    Button button5;
+    Button button1, button2, button3, button4, button5;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__diary_1);
+        addControl();
+        addEvents();
+    }
+
+    // Hàm để khai báo các thành phần được sử dụng trong activity
+    public void addControl()
+    {
+        hello_text = (TextView)findViewById(R.id.hello_text);
+        button1 = findViewById(R.id.add_diary_button_1);
+        button2 = findViewById(R.id.add_diary_button_2);
+        button3 = findViewById(R.id.add_diary_button_3);
+        button4 = findViewById(R.id.add_diary_button_4);
+        button5 = findViewById(R.id.add_diary_button_5);
+    }
+
+    // Hàm để thêm các event cho activity
+    public void addEvents()
+    {
+
+    }
+
+    // Hàm để thay đổi hình theme tự động theo thời gian
+    private void changeTheme()
+    {
+        int i = getTimeFromAndroid();
+        hello_text.setText(set_hello_text(i)+"\nLet's start with your mood");
+        if (i == 1 ) hello_text.setBackgroundResource(R.drawable.add_diary_1_activity_1_morning);
+        if (i == 2 ) hello_text.setBackgroundResource(R.drawable.add_diary_1_activity_2_afternoon);
+        if (i == 3 ) hello_text.setBackgroundResource(R.drawable.add_diary_1_activity_3_evening);
+        if (i == 4 ) hello_text.setBackgroundResource(R.drawable.add_diary_1_activity_4_night);
+    }
+
+    //Hàm lấy thời gian thực của máy
     private int getTimeFromAndroid() {
-
         Calendar rightNow = Calendar.getInstance();
         int hours = rightNow.get(Calendar.HOUR_OF_DAY); // return the hour in 24 hrs format (ranging from 0-23)
         System.out.println("harry porter"+hours);
@@ -35,6 +67,7 @@ public class Add_Diary_1Activity extends AppCompatActivity {
         return 4;
     }
 
+    //Hàm lấy nội dung câu chào theo thời gian buổi
     public String set_hello_text(int i)
     {
         if (i == 1 )
@@ -46,51 +79,4 @@ public class Add_Diary_1Activity extends AppCompatActivity {
         return "Good Night!";
     }
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add__diary_1);
-        int i = getTimeFromAndroid();
-         hello_text = (TextView)findViewById(R.id.hello_text);
-         hello_text.setText(set_hello_text(i)+"\nLet's start with your mood");
-         if (i == 1 ) hello_text.setBackgroundResource(R.drawable.add_diary_1_activity_1_morning);
-         if (i == 2 ) hello_text.setBackgroundResource(R.drawable.add_diary_1_activity_2_afternoon);
-         if (i == 3 ) hello_text.setBackgroundResource(R.drawable.add_diary_1_activity_3_evening);
-         if (i == 4 ) hello_text.setBackgroundResource(R.drawable.add_diary_1_activity_4_night);
-         button1 = findViewById(R.id.add_diary_button_1);
-         button2 = findViewById(R.id.add_diary_button_2);
-         button3 = findViewById(R.id.add_diary_button_3);
-         button4 = findViewById(R.id.add_diary_button_4);
-         button5 = findViewById(R.id.add_diary_button_5);
-
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-    }
 }
