4b9e8598d66479c15cae06c6a42ee7a7c726936b
Huysatoshi <156727huy@gmail.com>
2021-06-21 22:17:30 +0700

    complete feature change theme with 1 click

--- app/src/main/java/com/example/magicnote1/MainActivity2.java
+++ app/src/main/java/com/example/magicnote1/MainActivity2.java
@@ -23,7 +23,7 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
